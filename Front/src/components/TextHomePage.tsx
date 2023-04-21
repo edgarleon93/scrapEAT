c7af6261d2449e67b0bd1351a83b6b3163513383
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-04-21 13:56:45 +0200

    add text input and font sizing

---
+++ Front/src/components/TextHomePage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function TextHomePage() {
+  return (
+    <Typography variant="h1" align="center" sx={{ px: 46, pt: 35 }}>
+      Copie/paste the url you want us to scrap, we take care of the rest{" "}
+    </Typography>
+  );
+}
+export default TextHomePage;
--- Front/src/pages/Index.tsx
+++ Front/src/pages/Index.tsx
@@ -1,11 +1,24 @@
-import { Typography } from "@mui/material";
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, TextField, Typography } from "@mui/material";
 
 export function Index() {
   return (
-    <Typography variant="h1" align="center">
-      Copie/paste the url you want us to scrap, we take care of the rest{" "}
-    </Typography>
+    <>
+      <Typography variant="h1" align="center" sx={{ px: 5, pt: 30 }}>
+        Paste the url you want us to scrap,
+      </Typography>
+      <Typography variant="h1" align="center" sx={{ px: 5 }}>
+        we take care of the rest
+      </Typography>
+      <Box align="center" sx={{ mt: 10, px: 5 }}>
+        <TextField
+          fullWidth
+          id="standard-basic"
+          label="Past here"
+          variant="outlined"
+          size="small"
+        />
+      </Box>
+    </>
   );
 }
