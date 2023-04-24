30d050ecebe9ad67a65f3e45cd3037235291e1e7
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-04-24 10:26:26 +0200

    HomePage with nav text and textfield

--- Front/src/pages/HomePage.tsx
+++ Front/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import TextHomePage from "../components/TextHomePage";
 import TextInput from "../components/TextInput";
+import { useState } from "react";
 
 function HomePage() {
   return (
